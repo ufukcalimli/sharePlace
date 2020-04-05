36e9088aa16810c5f6ae30477e0083d8aae9a290
ufukcalimli <ufk.calimli@gmail.com>
2020-04-05 18:12:04 +0200

    Delete modal added

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -7,11 +7,23 @@ import Map from '../../shared/components/UIElements/Map/Map';
 
 import './PlaceItem.css';
 
-const PlaceItem = props => {
+const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
   const closeMapHandler = () => setShowMap(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    console.log('DELETING...');
+  };
 
   return (
     <React.Fragment>
@@ -26,6 +38,27 @@ const PlaceItem = props => {
         <div className='map-container'>
           <Map center={props.coordinates} zoom={16} />
         </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header='Are you sure?'
+        footerClass='place-item__modal-actions'
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              DELETE
+            </Button>
+          </React.Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that it can't be undone
+          thereafter.
+        </p>
       </Modal>
       <li className='place-item'>
         <Card className='place-item'>
@@ -42,7 +75,9 @@ const PlaceItem = props => {
               VIEW ON MAP
             </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
