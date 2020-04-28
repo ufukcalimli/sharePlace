234c972e40a5b08a67ea99ea01c5493d985fe579
ufukcalimli <ufk.calimli@gmail.com>
2020-04-28 18:35:27 +0200

    retrieving places bug fixed

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -43,16 +43,16 @@ const getPlaceById = (req, res, next) => {
   res.json({ place });
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
 
-  const place = DUMMY_PLACES.find((user) => user.creator === userId);
+  const places = DUMMY_PLACES.filter((user) => user.creator === userId);
 
-  if (!place) {
-    return next(new HttpError('Could not find place by place ID!', 404));
+  if (!places || places.length === 0) {
+    return next(new HttpError('Could not find places by user ID!', 404));
   }
 
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -94,7 +94,7 @@ const deletePlace = (req, res, next) => {
 };
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
