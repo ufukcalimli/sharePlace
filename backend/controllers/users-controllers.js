c6785d027f514978c1419012a21e02fb9b2ead75
ufukcalimli <ufk.calimli@gmail.com>
2020-04-29 17:43:40 +0200

    signup

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -2,6 +2,7 @@ const uuid = require('uuid/v4');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
 let DUMMY_USERS = [
   {
@@ -16,29 +17,40 @@ const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (errors.isEmpty()) {
-    throw new HttpError('Invalid input passed, please check the input', 422);
+    return next(new HttpError('Invalid input passed, please check the input', 422));
   }
 
-  const { name, email, password } = req.body;
+  const { name, email, password, places } = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError('Could not create user, email already exists', 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return next(new HttpError('500, Signing up failed', 500));
   }
 
-  const createdUser = {
-    id: uuid(),
+  if (existingUser) {
+    return next(new HttpError('422, User already exists!', 422));
+  }
+
+  const createdUser = new User({
     name,
     email,
+    image: 'imageUrl',
     password,
-  };
+    places,
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (error) {
+    return next(new HttpError('500, Signing up failed!', 500));
+  }
 
-  res.json(201).json({ user: createdUser });
+  res.json(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
@@ -49,7 +61,7 @@ const login = (req, res, next) => {
     throw new HttpError('Could not identify user, credentials seem to be wrong!', 401);
   }
 
-  res.json({ message: 'Looged in' });
+  res.json({ message: 'Logged in' });
 };
 
 exports.getUsers = getUsers;
