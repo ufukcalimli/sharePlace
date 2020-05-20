7c6e19a4683af495b3313319505794ca577f0f3c
ufukcalimli <ufukcalimli>
2020-05-20 02:13:28 +0200

    minor changes

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -33,7 +33,7 @@ const UserPlaces = () => {
       <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
-          <LoadingSpinner />
+          <LoadingSpinner asOverlay />
         </div>
       )}
       {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler}/>}
